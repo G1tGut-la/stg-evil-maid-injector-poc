2a5040115d2309cbad8fecf34e605a3c6d5ad589
tavog96 <31905549+tavog96@users.noreply.github.com>
2025-02-07 11:02:33 -0800

    Update bootload-env.sh

--- script/bootload-env.sh
+++ script/bootload-env.sh
@@ -12,34 +12,16 @@ fi
 echo "Updating system packages..."
 apt update && apt upgrade -y
 
-#echo "Installing required dependencies..."
+echo "Installing required dependencies..."
 apt install -y software-properties-common build-essential \
     zlib1g-dev libssl-dev libncurses5-dev libffi-dev libsqlite3-dev \
     libreadline-dev libbz2-dev liblzma-dev libgdbm-dev tk-dev \
     libdb-dev libexpat1-dev libmpdec-dev libgmp-dev curl wget
 
-echo "Downloading the latest Python source code..."
-PYTHON_VERSION=$(curl -s https://www.python.org/ftp/python/ | grep -oE '3\.[0-9]+\.[0-9]+' | sort -V | tail -n1)
-PYTHON_TAR="Python-$PYTHON_VERSION.tar.xz"
-PYTHON_SRC_DIR="/usr/local/src/Python-$PYTHON_VERSION"
+echo "Installing Python3..."
+apt install -y python3
 
-wget "https://www.python.org/ftp/python/$PYTHON_VERSION/$PYTHON_TAR" -P /usr/local/src
-tar -xf "/usr/local/src/$PYTHON_TAR" -C /usr/local/src
-
-echo "Building and installing Python $PYTHON_VERSION..."
-cd "$PYTHON_SRC_DIR"
-./configure --enable-optimizations
-make -j$(nproc)
-make altinstall
-
-# Check Python version
-PYTHON_BIN="/usr/local/bin/python${PYTHON_VERSION%.*}"
-echo "Python installed at: $PYTHON_BIN"
-$PYTHON_BIN --version
-
-echo "Ensuring pip and venv are installed..."
-$PYTHON_BIN -m ensurepip
-$PYTHON_BIN -m pip install --upgrade pip
+apt install python3-pip -y
 
 echo "Reviewing changes"
 echo "Python Version:"
